b982d461d3575cd35d58fb681885ec3b27f43f60
WCL <1140922677@qq.com>
2021-05-09 16:52:45 +0800

    Binder

---
+++ Assets/Scripts/PlayerScrpts/Binder.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class Binder : MonoBehaviour
+{
+    public Rigidbody2D myriRigidbody2D;
+    public Transform myTransform;
+    public Animator muAnim;
+    public float speed;
+
+    private void Update()
+    {
+        PlayerController.Instance.Update();
+    }
+
+    private void FixedUpdate()
+    {
+        PlayerController.Instance.InputControl();
+    }
+
+    private void Start()
+    {
+        PlayerController.Instance._rigidbody2D = myriRigidbody2D;
+        PlayerController.Instance._transform = myTransform;
+        PlayerController.Instance.movespeed = speed;
+        PlayerController.Instance._anim = muAnim;
+    }
+}
