8ffafc470ebbaf6fa05694e653ffd6b2df731dcd
Ambition_cng <1539792251@qq.com>
2021-05-13 17:36:36 +0800

    UI

--- Assets/Scripts/WeaponSystem/WeaponPool.cs
+++ Assets/Scripts/WeaponSystem/WeaponPool.cs
@@ -7,27 +7,32 @@ using TMPro;
 public class WeaponPool : MonoSingleton<WeaponPool>
 {
     public List<GameObject> weapons = new List<GameObject>();
+
+    public List<Animator> animators = new List<Animator>();
     public List<GameObject> weaponIcon = new List<GameObject>();
     public List<GameObject> weaponName = new List<GameObject>();
 
+    private int poolAmount;
     private int maxNum = 3;
     private int index;
 
     void Start()
     {
+        poolAmount = weapons.Count < maxNum ? weapons.Count : maxNum;
+        index = 0;
+
         InitializePool();
     }
 
     private void InitializePool()
     {
-        index = 0;
         for(int i = 0;i < weapons.Count;++i)
         {
             GameObject obj = Instantiate(weapons[i]);
             obj.transform.SetParent(transform);
         }
 
-        for(int i = 0;i < weapons.Count && i < maxNum;++i)
+        for(int i = 0;i < poolAmount;++i)
         {
             weaponIcon[i].GetComponent<Image>().sprite = weapons[i].GetComponent<Weapon>().weaponImage;
             weaponName[i].GetComponent<TMP_Text>().text = weapons[i].GetComponent<Weapon>().weaponName;
@@ -36,16 +41,21 @@ public class WeaponPool : MonoSingleton<WeaponPool>
 
     public GameObject FirstWeapon()
     {
-        BulletPool.Instance.currentWeapon = weapons[0];
-        return weapons[0];
+        BulletPool.Instance.currentWeapon = weapons[index];
+        animators[index].SetBool("isChosen", true);
+        return weapons[index];
     }
 
     public GameObject GetNextWeapon()
     {
-        index = (index + 1) % weapons.Count;
+        animators[index].SetBool("isChosen", false);
+
+        index = (index + 1) % poolAmount;
         GameObject obj = weapons[index];
         BulletPool.Instance.currentWeapon = obj;
         BulletPool.Instance.ChangeSprite();
+
+        animators[index].SetBool("isChosen", true);
 
         return obj;
     }
