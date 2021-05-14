b5c7bffbc2e0e7b33d5b2637f0485cd0f6bafdc8
Ambition_cng <1539792251@qq.com>
2021-05-14 13:08:36 +0800

    BagAnim

---
+++ Assets/Scripts/PlayerScripts/SwitchBagPanel.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchBagPanel : MonoBehaviour
+{
+    private Animator anim_child_01, anim_child_02;
+    private bool opposite = true;
+
+    void Awake()
+    {
+        anim_child_01 = GetComponentsInChildren<Animator>()[1];
+        anim_child_02 = GetComponentsInChildren<Animator>()[2];
+    }
+
+    void OnEnable()
+    {
+        anim_child_01.SetBool("push", opposite);
+        anim_child_02.SetBool("push", !opposite);
+    }
+
+    void Update()
+    {
+        if(Input.GetButtonDown("Horizontal"))
+        {
+            opposite = !opposite;
+            anim_child_01.SetBool("push", opposite);
+            anim_child_02.SetBool("push", !opposite);
+        }
+    }
+}
