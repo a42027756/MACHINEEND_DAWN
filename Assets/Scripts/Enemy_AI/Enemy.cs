ccd9406b2f81f62c75711b217dc1ff2913fd8623
WCL <1140922677@qq.com>
2021-05-21 21:47:53 +0800

    FSM Init

--- Assets/Scripts/Enemy_AI/Enemy.cs
+++ Assets/Scripts/Enemy_AI/Enemy.cs
@@ -5,9 +5,30 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     public int health;
-
+    protected FSM _fsm;   //创建状态机
+    protected Rigidbody2D _rigidbody2D;
+    protected Transform _transform;
+    private float flipEpsilon = 0.1f;
     public virtual bool TakeDamage(int damage)
     {
         return false;
+    }
+
+    protected void InitializeEnemy()
+    {
+        _transform = GetComponent<Transform>();
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    public void Flip()
+    {
+        if (_rigidbody2D.velocity.x < -flipEpsilon)
+        {
+            _transform.localRotation = Quaternion.Euler(0, 0, 0);
+        }
+        else if (_rigidbody2D.velocity.x > flipEpsilon)
+        {
+            _transform.localRotation = Quaternion.Euler(0, 180, 0);
+        }
     }
 }
--- Assets/Scripts/Enemy_AI/FlyingEnemy.cs
+++ Assets/Scripts/Enemy_AI/FlyingEnemy.cs
@@ -3,25 +3,50 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Experimental.Rendering.Universal;
+using Random = UnityEngine.Random;
 
 public class FlyingEnemy : Enemy
 {
-    private FSM _fsm;   //创建状态机
+    public PolygonCollider2D sight;     //视觉触发器
+    public CircleCollider2D hearing;    //听觉触发器
+    public float flyingspeed;           //飞行速度
+    public float waitTime;              //巡逻间隙等待时间
+    [SerializeField]private float nowTime;              //现在等待的时间
+    
+
+    private Vector3 movePos;    //目的地
+    private Vector3 moveVelocity;  //移动速度(矢量)
+    private float epsilon = 0.1f;
     
     //声明State
     private State idleState;
+    private State moveState;
 
-    void Start()
+    private void Start()
+    {
+        InitializeEnemy();
+
+        InitState();
+        
+        _fsm = new FSM(idleState);
+    }
+
+    private void InitState()
     {
         idleState = new State("idle");
-        _fsm = new FSM(idleState);
+        moveState = new State("move");
         
+        idleState.ONEnterHandler = Idle_Enter;
         idleState.ONActionHandler = Idle_Action;
-        
+
+        moveState.ONEnterHandler = Move_Enter;
+        moveState.ONActionHandler = Move_Action;
+        moveState.ONExitHandler = Move_Exit;
     }
     
-    void Update()
+    private void Update()
     {
+        Flip();
         if (_fsm != null)
         {
             _fsm.Update();
@@ -54,12 +79,81 @@ public class FlyingEnemy : Enemy
         }
     }
     
-    //===========idle============
+    //========================idle========================
+    //replace  idle:Enter:
+    private void Idle_Enter(State _from, State _to)
+    {
+        Debug.Log("Idle_Enter");
+        nowTime = waitTime;
+    }
     //replace idle:Action:
-    void Idle_Action(State _curState)
+    private void Idle_Action(State _curState)
     {
         Debug.Log("Camerator idle");
+        if (Wait(ref nowTime))
+        {
+            Debug.Log("Wait Finish");
+            _fsm.ChangeState(moveState);
+        }
     }
-    //==========================
+
+    private bool Wait(ref float time)
+    {
+        time -= Time.deltaTime;
+        if (time <= epsilon)
+        {
+            return true;
+        }
+        return false;
+    }
+    //==============================================
+    
+    //=====================move=====================
+    private void Move_Enter(State _from, State _to)
+    {
+        Debug.Log("Enter Move");
+        movePos = FindRandPoint(2f,2f);
+    }
+
+    private void Move_Action(State _current)
+    {
+        Debug.Log("Move Action");
+        if (Move2Point(movePos))
+        {
+            _fsm.ChangeState(idleState);
+        }
+    }
+
+    private void Move_Exit(State _from, State _to)
+    {
+        Debug.Log("Exit Move");
+        _rigidbody2D.velocity = new Vector2(0, 0);
+    }
+    
+    //寻找附近随机点
+    Vector3 FindRandPoint(float x,float y)
+    {
+        Debug.Log("FindRandomPoint");
+        return _transform.position + new Vector3(Random.Range(-x, x), Random.Range(-y, y), 0);
+        
+    }
+    
+    //移动至固定地点
+    private bool Move2Point(Vector3 movePoint)
+    {
+        Debug.Log("Move");
+        moveVelocity = (movePos - _transform.position).normalized;
+        if ((movePoint - _transform.position).magnitude > epsilon)
+        {
+            _rigidbody2D.velocity = moveVelocity * flyingspeed;
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+        
+    }
+    //==============================================
 }
 
