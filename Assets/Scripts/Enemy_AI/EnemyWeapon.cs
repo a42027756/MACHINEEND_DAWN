8a8e024f6fb564a61e89282988649add5aa3fd3a
Ambition_cng <1539792251@qq.com>
2021-05-26 20:40:29 +0800

    enemy weapon

--- Assets/Scripts/Enemy_AI/EnemyWeapon.cs
+++ Assets/Scripts/Enemy_AI/EnemyWeapon.cs
@@ -7,13 +7,16 @@ public class EnemyWeapon : MonoBehaviour
     public GameObject bulletPre;
     public Sprite bulletSprite;
     private Transform muzzle_01, muzzle_02;
-    private Vector2 firePoint_01, firePoint_02;
+    private Vector2 firePoint;
     private GameObject player;
+    private Transform enemy;
     private SpriteRenderer spriteRenderer;
-    public bool isFired;
+    private bool isFired;
+    private bool fireSequence;
+    [SerializeField] private float angleOffset;
     [SerializeField] private float damage;
     [SerializeField] private float interval;
-    [SerializeField] private float timeCounter;
+    private float timeCounter;
     [SerializeField] private float bulletSpeed;
     
     void Start()
@@ -22,8 +25,11 @@ public class EnemyWeapon : MonoBehaviour
         muzzle_01 = GetComponentsInChildren<Transform>()[1];
         muzzle_02 = GetComponentsInChildren<Transform>()[2];
         player = GameObject.FindGameObjectWithTag("Player");
+        enemy = GetComponentsInParent<Transform>()[1];
+        Debug.Log(enemy.name);
 
         spriteRenderer = GetComponent<SpriteRenderer>();
+        fireSequence = false;
     }
 
     public void FireDecision()
@@ -48,14 +54,15 @@ public class EnemyWeapon : MonoBehaviour
     }
     private void Fire()
     {
-        firePoint_01 = muzzle_01.position;
+        firePoint = fireSequence ? muzzle_01.position : muzzle_02.position;
+        fireSequence = !fireSequence;
+        
         EnemyBulletPool.Instance.bulletSprite = bulletSprite;
         EnemyBulletPool.Instance.ChangeSprite();
-        Vector2 playerPos = player.transform.position;
-        Vector2 target = playerPos - firePoint_01;
+        Vector2 target = player.transform.position - transform.position;
 
         GameObject bullet = EnemyBulletPool.Instance.GetFromPool();
-        bullet.transform.position = firePoint_01;
+        bullet.transform.position = firePoint;
         bullet.GetComponent<Enemy_Bullet>().bulletEnemyDamage = damage;
 
         float rotateByZ = Mathf.Atan2(target.y, target.x) * Mathf.Rad2Deg;
@@ -66,7 +73,17 @@ public class EnemyWeapon : MonoBehaviour
     public void WeaponRotation()
     {
         Vector2 difference = player.transform.position - transform.position;
-        float rotateByZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
-        transform.rotation = Quaternion.Euler(0, 0, rotateByZ);
+        float angle = Vector2.Angle(Vector2.up, difference);
+        float rotateAngle = angle - angleOffset;
+        if(enemy.position.x < player.transform.position.x)
+        {
+            enemy.GetComponent<SpriteRenderer>().flipX = true;
+            transform.rotation = Quaternion.Euler(0, 180, rotateAngle);
+        }
+        else
+        {
+            enemy.GetComponent<SpriteRenderer>().flipX = false;
+            transform.rotation = Quaternion.Euler(0, 0, rotateAngle);
+        }
     }
 }
