277e3382bcc151d3377b99ab3be15d7df3af618d
WCL <1140922677@qq.com>
2021-05-27 19:13:59 +0800

    Debug FlyingEnemy

---
+++ Assets/Scripts/Environment/ShowBuffMessage.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShowBuffMessage : MonoBehaviour
+{
+    public GameObject messagePanel;
+    
+
+    public List<string> messages = new List<string>();
+
+    private bool isShowing;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ShowPanel();
+    }
+
+    private void ShowPanel()
+    {
+        ChangeMessage();
+        if (GTime.Instance.GetGtime() == 6 && !isShowing)
+        {
+            messagePanel.SetActive(true);
+        }
+    }
+
+    private void ChangeMessage()
+    {
+        messagePanel.GetComponentsInChildren<Text>()[0].text = "Day " + (GTime.Instance.pass_day+1);
+        messagePanel.GetComponentsInChildren<Text>()[1].text = messages[GTime.Instance.pass_day];
+    }
+
+    public void ClosePanel()
+    {
+        messagePanel.SetActive(false);
+        isShowing = false;
+    }
+}
